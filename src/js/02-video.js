587f96058cc3852902b568a89bc4460d31288c17
Diachenko Oleksandr <palllich@gmail.com>
2022-09-20 23:58:25 +0300

    make 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player('vimeo-player');
+const savedTime = localStorage.getItem('videoplayer-current-time') || 0;
+
+player.setCurrentTime(savedTime);
+player.on('timeupdate', throttle(setTime, 1000));
+
+function setTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  //   console.log(savedTime);
+}
